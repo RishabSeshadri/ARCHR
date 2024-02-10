845b46fb0c3909221c9efd074062f94f993233d9
Rishab Seshadri <rishab.seshadri@gmail.com>
2024-02-10 15:06:28 -0500

    Trying to find by address

--- archr.py
+++ archr.py
@@ -3,8 +3,12 @@ from bleak import BleakScanner
 
 async def main():
     devices = await BleakScanner.discover()
-    for d in devices:
-        print(d.address.strip())  
+    print(await BleakScanner.find_device_by_address("B0:BE:83:4B:F1:B8"))
+    #for d in devices:
+
+        # DC:B5:4F:AB:CC:8C me
+        # B0:BE:83:4B:F1:B8 carlos
+        # print("[", d.address.strip(), "]")      
             
 
 asyncio.run(main())
