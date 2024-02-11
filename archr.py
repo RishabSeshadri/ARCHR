c14d91454d7969c859a1fd4417a8f681401cbb5d
Rishab Seshadri <rishab.seshadri@gmail.com>
2024-02-11 03:32:37 -0500

    Mid-README changes

--- archr.py
+++ archr.py
@@ -2,20 +2,39 @@
 import asyncio
 from bleak import BleakScanner
 import aiohttp
-import asyncio
-from bleak import BleakScanner
 import requests
 
-async def main():
-    while True:
-        deviceName = input('Enter device name: ')
-        deviceName = deviceName.strip().replace("’","'")
-        cardNumber = input('Enter card number (sample): ')
+from flask import Flask, request, render_template, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+async def home():
+    if request.method == 'POST':
+        deviceName = request.form['deviceName']
         res = await BleakScanner.find_device_by_name(deviceName)
-        
+    
         if res != None:
-            str = "http://34.138.254.116:80/add/" + res.address + "/" + cardNumber
+            str = "http://34.138.254.116:80/add/" + res.address + "/" + deviceName
             requests.get(str)
+            return f"Successfully added {deviceName}."
+
+        return f"Failed at adding '{deviceName}'. The device was not found."
+    else:
+        return render_template('bank.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+async def main():
+    deviceName = deviceName.strip().replace("’","'")
+    res = await BleakScanner.find_device_by_name(deviceName)
+    
+    if res != None:
+        str = "http://34.138.254.116:80/add/" + res.address + "/" + deviceName
+        requests.get(str)
             
 
     async with aiohttp.ClientSession() as session:
@@ -34,3 +53,4 @@ async def main():
             await asyncio.sleep(1)
 
 asyncio.run(main())
+"""
